1875af162008a68aa48159102e9b19a9456d0645
boxuanhu <boxuanhu@tamu.edu>
2023-02-20 21:45:23 -0600

    implement filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,12 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    if params[:movie]
+      # @movies = Movie.rating(params[:rating])
+      @movies = Movie.search(params[:movie][:rating])
+    else
+      @movies = Movie.order('release_date DESC')
+    end
   end
 
   # GET /movies/1 or /movies/1.json
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+
+    def self.search(search)
+        where("rating LIKE ?","%#{search}")
+    end
 end
